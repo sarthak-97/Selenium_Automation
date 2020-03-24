60dcc17ba33ac49bb71a21c98396c88ef4ab1f52
sarthak-97 <sarthakgupta115@gmail.com>
2020-03-24 10:28:04 +0530

    automation for register link

--- src/test/java/org/java/selenium/test/InvokeBrowser.java
+++ src/test/java/org/java/selenium/test/InvokeBrowser.java
@@ -1,7 +1,16 @@
 package org.java.selenium.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+/**
+ * 
+ * @author sarthak-pc
+ *
+ */
 
 public class InvokeBrowser {
 
@@ -12,10 +21,37 @@ public class InvokeBrowser {
 		WebDriver driver = new ChromeDriver();
 		
 		
-		driver.get("https://www.amazon.in");
+		driver.get("https://demo.opencart.com");
+		
+
+		
+		WebElement accountLink = driver.findElement(new By.ByXPath("/html/body/nav/div/div[2]/ul/li[2]/a"));
+		if(accountLink.isEnabled()) {
+			System.out.println("link found");
+			
+					
+			accountLink.click();
+			
+			
+			WebElement regLink = driver.findElement(new By.ByLinkText("Register"));
+			if(regLink.isEnabled()) {
+				System.out.println(" register link found");
+				
+						
+				regLink.click();
+			}
+			
+			
+		}
+		
+
+		
 		
 		System.out.println(	driver.getTitle());
 		
+	/**
+	 ******** for pausing the browser screen 
+	 */
 		System.console().readLine();
 		driver.close();
 
